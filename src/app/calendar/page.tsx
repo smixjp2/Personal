dcf9ff874586d7f9102411a2a7e605dd200e2bf4
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-27 14:15:28 +0000

    Je veux un calendrier simple

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -3,10 +3,5 @@
 import { EventCalendar } from "@/components/calendar/event-calendar";
 
 export default function CalendarPage() {
-  // Use negative margins to occupy the full space provided by the main layout
-  return (
-    <div className="-m-4 sm:-m-6 lg:-m-8">
-      <EventCalendar />
-    </div>
-  );
+  return <EventCalendar />;
 }
